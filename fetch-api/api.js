3aa2268b6f7d3e20b1cd1923767cfd2099323a4d
mohammedkhaled <mohdkhaled1031@gmail.com>
2025-10-24 11:37:57 +0530

    completed the api project

---
+++ fetch-api/api.js
@@ -0,0 +1,35 @@
+fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then(console.log);
+
+
+
+
+async function fetchApi() {
+    let a = await fetch('https://dummyjson.com/products')
+    let b = await a.json()
+    for(const element of b.products){
+
+        let div = document.createElement('div')
+        let h1 = document.createElement('h1')
+        let p = document.createElement('p')
+        let p2 = document.createElement('p')
+        let b = document.createElement('b')
+      
+
+        h1.textContent=element.title
+        p.textContent=element.description
+        p2.textContent=element.category
+        b.textContent=element.price
+
+        div.appendChild(h1)
+        div.appendChild(p)
+        div.appendChild(p2)
+        div.appendChild(b)
+
+        document.querySelector('#parent').appendChild(div)
+    }
+    
+}
+
+fetchApi()
