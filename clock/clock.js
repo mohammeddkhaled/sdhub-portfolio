261af80dd1f033f503f9a368850509b707d42bba
mohammedkhaled <mohdkhaled1031@gmail.com>
2025-10-26 15:11:05 +0530

    added date in it

--- clock/clock.js
+++ clock/clock.js
@@ -1,10 +1,16 @@
 const hours = document.querySelector('#hours')
 const min = document.querySelector('#min')
 const second = document.querySelector('#sec')
+const date = document.querySelector('#date')
+const month = document.querySelector('#month')
+const year = document.querySelector('#year')
 
 setInterval(() => {
     let time = new Date()
-
+    date.innerHTML = time.getDate()
+    month.innerHTML = time.getMonth()
+    year.innerHTML = time.getFullYear()
+    
     hours.innerHTML = time.getHours()
     min.innerHTML = time.getMinutes()
     second.innerHTML = time.getSeconds()
